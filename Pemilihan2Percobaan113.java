e826b08d10aea00a7efd72011a4b3d0977c220c6
m-fatahillah <fatahcrown789@gmail.com>
2024-10-03 13:44:06 +0700

    update

--- Pemilihan2Percobaan113.java
+++ Pemilihan2Percobaan113.java
@@ -2,17 +2,29 @@ import java.util.Scanner;
 
 public class Pemilihan2Percobaan113{
     public static void main(String[] args) {
+        //deklarasi
         Scanner input13 = new Scanner(System.in);
 
+        //input
         System.out.print("Masukkan tahun : ");
         int tahun = input13.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) !=0)
-                System.out.println("Tahun Kabisat");
-                
-        } else
-                System.out.println("Bukan Tahun Kabisat");
-                    
+            if ((tahun % 100) !=0) {
+                System.out.println("Tahun Kabisat");      
+            }else
+                if (tahun%400 == 0) 
+                    System.out.println("Tahun Kabisat");                    
+                else 
+                    System.out.println("Bukan Tahun Kabisat");     
+        } 
+        else {
+            System.out.println("Bukan Tahun Kabisat");
     }
+                       
+    }
+
 }
+
+
+
