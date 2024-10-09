e7d28d90b93e4ef0a5bf5c6bb9826324dd7be4bd
m-fatahillah <fatahcrown789@gmail.com>
2024-10-09 15:36:07 +0700

    Add files via upload

---
+++ tugas1.java
@@ -0,0 +1,26 @@
+public class tugas1 {
+    public static void main(String[] args) {
+        int bill1 = 20, bill2 = 54, bill3 = 15, billmax = 0;
+
+        if (bill1 > bill2) {
+            if (bill1 > bill3) {
+                billmax = bill1;
+            }
+            else {
+                billmax = bill3;
+            }
+        }
+        else {
+            if (bill2 > bill3) {
+                billmax = bill2;
+            }
+            else{
+                billmax = bill3;
+            }
+        }
+
+        System.out.println("billmax : " + billmax);
+
+    }
+    
+}
